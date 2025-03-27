2e4d8fc164362cdee84ed2ad594b0aea2e330d40
José Eduardo <limaedu18@hotmail.com>
2025-03-27 19:48:15 -0300

    Adição: metódos de busca por ID e delete

--- src/main/java/com/j_edulima/accouting/controller/CompanyController.java
+++ src/main/java/com/j_edulima/accouting/controller/CompanyController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,20 +31,38 @@ public class CompanyController {
 	private CompanyService service;
 
 	@PostMapping("/{id}")
-	@Operation(summary = "", description = "")
+	@Operation(summary = "Registra uma empresa", description = "Registra uma empresa no ID de um usuário")
 	public ResponseEntity<Void> register(@RequestBody CompanyRequestDTO companyRequestDTO,
 			@PathVariable("id") Long id) {
 		service.register(companyRequestDTO, id);
+
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
+	@DeleteMapping("/{id}")
+	@Operation(summary = "Deleta uma empresa registrada", description = "Deleta uma empresa registrada com base no ID")
+	public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+		service.delete(id);
+
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+
 	@GetMapping
-	@Operation(summary = "", description = "")
+	@Operation(summary = "Busca todas as empresas", description = "Buscas todas as empresas registradas no DB")
 	public ResponseEntity<List<CompanyResponseDTO>> findAll() {
 		List<Company> companys = service.findAll();
-		List<CompanyResponseDTO> companyDTOs = companys.stream().map(x -> new CompanyResponseDTO(x)).collect(Collectors.toList());
+		List<CompanyResponseDTO> companyDTOs = companys.stream().map(x -> new CompanyResponseDTO(x))
+				.collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(companyDTOs);
+	}
+
+	@GetMapping("/{id}")
+	@Operation(summary = "Busca uma empresa", description = "Busca uma empresa com base no ID fornecido")
+	public ResponseEntity<CompanyResponseDTO> find(@PathVariable("id") Long id) {
+		Company company = service.findById(id);
+
+		return ResponseEntity.ok().body(new CompanyResponseDTO(company));
 	}
 
 }
--- src/main/java/com/j_edulima/accouting/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/j_edulima/accouting/service/impl/CompanyServiceImpl.java
@@ -18,34 +18,31 @@ public class CompanyServiceImpl implements CompanyService {
 
 	@Autowired
 	private CompanyRepository repository;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Override
 	public void register(CompanyRequestDTO companyDTO, Long id) {
 		User user = userRepository.findById(id).orElseThrow();
 		Company company = CompanyMapper.toEntity(companyDTO, user);
-		
 		repository.save(company);
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-
+		repository.deleteById(id);
 	}
 
 	@Override
 	public List<Company> findAll() {
-		
 		return repository.findAll();
 	}
 
 	@Override
 	public Company findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Company company = repository.findById(id).orElseThrow();
+		return company;
 	}
 
 }
